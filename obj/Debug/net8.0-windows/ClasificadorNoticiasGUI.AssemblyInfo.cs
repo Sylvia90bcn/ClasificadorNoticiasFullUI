f3a00602a8d1924d00cbbec8422e0e2eea00de34
Silvia <sylvia.90bcn@gmail.com>
2025-10-27 22:38:19 +0100

    primera grafica dataset

--- obj/Debug/net8.0-windows/ClasificadorNoticiasGUI.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ClasificadorNoticiasGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClasificadorNoticiasGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b693089a96fa833fad74dade12bb1aa482ec34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3227ed69a000f9ba083cbe6e74a91265a422c239")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClasificadorNoticiasGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClasificadorNoticiasGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
