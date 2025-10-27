f96153852b4776afb756e35fe304779235b5a6a0
Silvia <sylvia.90bcn@gmail.com>
2025-10-27 19:57:44 +0100

    añadir al dataset

--- obj/Debug/net8.0-windows/ClasificadorNoticiasGUI.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ClasificadorNoticiasGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClasificadorNoticiasGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1055d2faa544872cf5343d4f96ede9ffd66845f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64045c5adb6a021cce1ca1350b5ccb8b932cab34")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClasificadorNoticiasGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClasificadorNoticiasGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
