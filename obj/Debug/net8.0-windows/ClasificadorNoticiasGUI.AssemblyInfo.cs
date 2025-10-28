c0c1d54b00aec4921e4bac001e0424b4ddb0aa03
Silvia <sylvia.90bcn@gmail.com>
2025-10-28 22:06:18 +0100

    tiempo ms

--- obj/Debug/net8.0-windows/ClasificadorNoticiasGUI.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ClasificadorNoticiasGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClasificadorNoticiasGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49351d08fdad08f0c4c299bfae2cc9a11771114b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc779083cbcec33cfc7642b072804619624b684")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClasificadorNoticiasGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClasificadorNoticiasGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
